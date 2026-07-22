9526bad91bceaf6be603ef360f39263c203dc4c5
raghavreddy8 <raghavreddyb8@gmail.com>
2026-07-23 00:26:28 +0530

    changed the backedn url to render url

--- WHENJ-NOW/frontend/scripts/api.js
+++ WHENJ-NOW/frontend/scripts/api.js
@@ -1,5 +1,5 @@
 // Replace the RENDER_BACKEND_URL with your actual Render URL after deploying your backend
-const RENDER_BACKEND_URL = "https://your-backend-app.onrender.com";
+const RENDER_BACKEND_URL = "https://whenj-now.onrender.com";
 const LOCAL_BACKEND_URL = "http://127.0.0.1:8000";
 
 const isLocalhost = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
