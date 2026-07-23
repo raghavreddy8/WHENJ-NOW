cac253faf05bc753df54772b962d98270bff5fb4
raghavreddy8 <raghavreddyb8@gmail.com>
2026-07-23 07:50:39 +0530

    testing scheduler-5

--- WHENJ-NOW/news_intelligence/app/scheduler/scheduler.py
+++ WHENJ-NOW/news_intelligence/app/scheduler/scheduler.py
@@ -11,7 +11,7 @@ scheduler = BackgroundScheduler(timezone=ist)
 
 # Run at 7:00, 12:00, 17:00, 21:00 (system local time — IST)
 scheduler.add_job(run_pipeline, "cron", hour=7,  minute=0, id="earlymorning")
-scheduler.add_job(run_pipeline, "cron", hour=7,  minute=51, id="morning")
+scheduler.add_job(run_pipeline, "cron", hour=7,  minute=53, id="morning")
 scheduler.add_job(run_pipeline, "cron", hour=12, minute=0, id="noon")
 scheduler.add_job(run_pipeline, "cron", hour=15, minute=0, id="afternoon")
 scheduler.add_job(run_pipeline, "cron", hour=18, minute=0, id="evening")
